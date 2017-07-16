9b119c77c79e9b98fb706295de8f6d8448df30f9
Law, Sean <Sean.Law@tdameritrade.com>
2017-07-15 20:38:58 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # joy
+
+Geoms to make joy plots using bokeh, written by Sean M. Law
+
+Inspired by Claus Wilke's [ggjoy](https://github.com/clauswilke/ggjoy), the code provided here contains a minimal example for creating a joy plot comparing movie lengths. The key differntiator in this work is the interactive slider that allows the user to define the vertical spacing between each plot.
+
+# Requirements
+
+```
+conda install bokeh
+conda install pandas
+conda install numpy
+conda install scipy
+```
+
+# Usage
+
+```
+bokeh serve --show joy.py
+```
---
+++ joy.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource, Slider
+from bokeh.layouts import layout
+from bokeh.io import curdoc
+from scipy.stats import gaussian_kde
+
+raw_df = pd.read_csv('movies.csv', index_col=0)
+raw_df = raw_df[raw_df['year'] >= 1920]
+raw_df.head()
+
+x = np.linspace(-100, 200, 51)
+groups = raw_df.groupby('year')
+df = pd.DataFrame(index=x)
+for year, group in groups:
+    dat = group['length']
+    pdf = gaussian_kde(dat.values)
+    df[str(year)] = pdf(x)
+
+def get_movie_data(df, spacing=0.0):
+    n = len(df.columns)
+    data = dict(
+                xs=np.vsplit(np.array([df.index.values,]*n), n),  # Repeat index n times
+                ys=np.vsplit(df.T.values + (np.array(range(n)) * spacing).reshape(n,1), n)
+            )
+    return data
+
+cds = ColumnDataSource(get_movie_data(df))
+
+#p = figure(plot_width=1200, plot_height=500)
+p = figure()
+p.patches(xs='xs', ys='ys', source=cds, fill_color='grey')
+p.multi_line(xs='xs', ys='ys', source=cds, color='black')
+
+def slider_update(attrname, old, new):
+    spacing = s.value
+    cds.data = get_movie_data(df, spacing)
+    
+s = Slider(start=0.0, end=0.06, value=0.0, step=.0001)
+s.on_change('value', slider_update)
+
+l = layout([[p], [s]], sizing_mode='stretch_both')
+
+curdoc().add_root(l)
